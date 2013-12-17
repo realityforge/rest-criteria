9d9a552b85a2226312b93af91390b0ba0637d018
Hai Huynh <hai@stocksoftware.com.au>
2013-12-17 17:39:36 +1100

    Added #getPathAsString()

--- src/main/java/org/realityforge/rest_criteria/model/VariableExpression.java
+++ src/main/java/org/realityforge/rest_criteria/model/VariableExpression.java
@@ -17,4 +17,22 @@ public class VariableExpression
   {
     return _path;
   }
+
+  @Nonnull
+  public String getPathAsString()
+  {
+    if ( _path.length == 0 )
+    {
+      return "";
+    }
+
+    final StringBuilder b = new StringBuilder();
+    b.append( _path[ 0 ] );
+    for ( int i = 1; i < _path.length; i++ )
+    {
+      b.append( "." ).append( _path[ i ] );
+    }
+    return b.toString();
+  }
+
 }
